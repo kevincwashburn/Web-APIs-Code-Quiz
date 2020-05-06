945773e3b4d60aa3eeaaaf584667485c1f286774
Kevin Washburn <kevincwashburn@gmail.com>
2020-05-06 10:28:41 -0400

    fixed button bugs in highscore-script.js

--- Assets/JS/highscore-script.js
+++ Assets/JS/highscore-script.js
@@ -1,31 +1,22 @@
-var btnGoBack = document.getElementById("btn-go-back");
-var btnClearScores = document.getElementById("btn-clear-scores");
-var highScoresList = document.getElementById("high-scores-list");
+let btnGoBack = $("#btn-go-back")
+let btnClearScores = $("#btn-clear-scores")
+let highScoresList = $("#high-scores-list")
 
 
 function renderHighScores() {
-    
-    // render a new li for each high score input
     for(var i = 0; i < highScores.length; i++) {
-        // var score = highScores[i];
-        // 1. KCW 24
-            console.log(highScores[i])
+            // console.log(highScores[i])
         var li = document.createElement("li");
         li.textContent = highScores[i].initials.toUpperCase() + " " + highScores[i].score;
-
         highScoresList.appendChild(li);
     }
 }
 
 function getScore() {
     var storedScore = JSON.parse(localStorage.getItem("highScores") || "[]");
-
     if(storedScore !== null) {
         highScores = storedScore;
     }
-
-    
-    
     renderHighScores();
 }
 
